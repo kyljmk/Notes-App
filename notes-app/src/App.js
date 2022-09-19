d2dbe83fdf266b1c24ce1444b6d0e14598196f65
kyljmk <k.mansonkullin@googlemail.com>
2022-09-19 22:50:30 +0200

    Add App Component

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,10 +1,43 @@
 import "./App.css";
+import Editor from "./Editor";
+import Sidebar from "./Sidebar";
+import Split from "react-split";
+import { nanoid } from "nanoid";
+import { useState } from "react";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [currentNoteId, setCurrentNoteId] = useState(
+    (notes[0] && notes[0].id) || ""
+  );
+
+  const createNewNote = () => {
+    const newNote = {
+      id: nanoid(),
+      body: "# Type your markdown notes title here",
+    };
+    setNotes((prevNotes) => [newNote, ...prevNotes]);
+    setCurrentNoteId(newNote.id);
+  };
+
+  console.log(notes);
+
   return (
-    <div className="App">
-      <h1>Notes</h1>
-    </div>
+    <main>
+      {notes.length > 0 ? (
+        <Split sizes={[30, 70]} direction="horizontal" className="split">
+          <Sidebar />
+          {currentNoteId && notes.length > 0 && <Editor />}
+        </Split>
+      ) : (
+        <div className="no-notes">
+          <h1>You have no notes</h1>
+          <button className="first-note" onClick={createNewNote}>
+            Create one now
+          </button>
+        </div>
+      )}
+    </main>
   );
 }
 
---
+++ notes-app/src/Editor.js
@@ -0,0 +1,8 @@
+import ReactMde from "react-mde";
+import Showdown from "showdown";
+
+function Editor() {
+  return <div>Editor</div>;
+}
+
+export default Editor;
